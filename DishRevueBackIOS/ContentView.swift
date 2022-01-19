b6f136f1e683b00175a27fb42649bac65e7bb858
Lillo <lillofriscia@gmail.com>
2022-01-19 11:13:29 +0100

    Initial Commit

---
+++ DishRevueBackIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DishRevueBackIOS
+//
+//  Created by Calogero Friscia on 19/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DishRevueBackIOS/DishRevueBackIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishRevueBackIOSApp.swift
+//  DishRevueBackIOS
+//
+//  Created by Calogero Friscia on 19/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DishRevueBackIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
