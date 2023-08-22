8959ad7f49fff4bad1df3d9b85b38189d2cd942a
Lillo <lillofriscia@gmail.com>
2023-08-22 16:51:16 +0200

    v.1.0(119) Settato trenino d'apertura. Auth - UserName - Fetch - LandingPage/MainView

---
+++ DishRevueBackIOS/Views/NuovaProprietà/NewPropertyLandingPage.swift
@@ -0,0 +1,18 @@
+//
+//  NewPropertyLandingPage.swift
+//  DishRevueBackIOS
+//
+//  Created by Calogero Friscia on 19/08/23.
+//
+
+import SwiftUI
+
+struct NewPropertyLandingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewPropertyLandingPage()
+}
