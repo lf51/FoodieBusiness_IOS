2ce10b647ea710477b420d6e431121f0d97269a1
Lillo <lillofriscia@gmail.com>
2023-08-27 22:41:13 +0200

    v.1.0(120) -> Configurato trenino auth - fetch data con waiting view in overlay. Da ottimizzare

---
+++ DishRevueBackIOS/TEST/TestFILTRABLE.swift
@@ -0,0 +1,18 @@
+//
+//  TestFILTRABLE.swift
+//  DishRevueBackIOS
+//
+//  Created by Calogero Friscia on 24/08/23.
+//
+
+import SwiftUI
+
+struct TestFILTRABLE: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestFILTRABLE()
+}
