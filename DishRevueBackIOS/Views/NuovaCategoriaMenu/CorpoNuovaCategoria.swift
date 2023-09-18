88a158e8b0a2af1784e204f9c5526fd7fdf13411
Lillo <lillofriscia@gmail.com>
2023-09-18 21:55:41 +0200

    v.1.0(126)

---
+++ DishRevueBackIOS/Views/NuovaCategoriaMenu/CorpoNuovaCategoria.swift
@@ -0,0 +1,18 @@
+//
+//  CorpoNuovaCategoria.swift
+//  DishRevueBackIOS
+//
+//  Created by Calogero Friscia on 15/09/23.
+//
+
+import SwiftUI
+
+struct CorpoNuovaCategoria: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CorpoNuovaCategoria()
+}
